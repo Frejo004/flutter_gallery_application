22ad4e7935a8040dd07e31bc9ba08374c83817b2
Frejus DASSI <frejohope@gmail.com>
2025-02-04 12:19:42 +0100

    Refactor MainController to use separate Gallery and Categories widgets

--- lib/main.dart
+++ lib/main.dart
@@ -37,23 +37,45 @@ class MainController extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          child: Text("Gallery",
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.green[500],
-            fontWeight: FontWeight.w900,
-          )),
-        ),
-        Container(
-          child: Text("Categories",
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.red[500],
-            fontWeight: FontWeight.w900,
-          )),
-        ),
+        Gallery(),
+        Categories(),
       ],
+    );
+  }
+}
+
+class Categories extends StatelessWidget {
+  const Categories({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Categories",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.red[500],
+        fontWeight: FontWeight.w900,
+      )),
+    );
+  }
+}
+
+class Gallery extends StatelessWidget {
+  const Gallery({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Gallery",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.green[500],
+        fontWeight: FontWeight.w900,
+      )),
     );
   }
 }
