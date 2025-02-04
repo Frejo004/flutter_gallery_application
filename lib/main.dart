e8e8ed1e1f2644b9a6b12cd4a419c5e083bec9c1
Frejus DASSI <frejohope@gmail.com>
2025-02-04 10:40:26 +0100

    Ajout de fichiers de configuration pour Flutter

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+sfsfsdfs
