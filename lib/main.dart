0baec3aa1357ad72e9522d5f50af88fc68693694
Frejus DASSI <frejohope@gmail.com>
2025-02-04 12:26:24 +0100

    Suppression des classes MainController, Categories et Gallery dans main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,58 +27,6 @@ class MyApp extends StatelessWidget {
 
         body: MainController(),
       ) ,
-    );
-  }
-}
-
-class MainController extends StatelessWidget {
-  const MainController({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Gallery(),
-        Categories(),
-      ],
-    );
-  }
-}
-
-class Categories extends StatelessWidget {
-  const Categories({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text("Categories",
-      style: TextStyle(
-        fontSize: 30,
-        color: Colors.red[500],
-        fontWeight: FontWeight.w900,
-      )),
-    );
-  }
-}
-
-class Gallery extends StatelessWidget {
-  const Gallery({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text("Gallery",
-      style: TextStyle(
-        fontSize: 30,
-        color: Colors.green[500],
-        fontWeight: FontWeight.w900,
-      )),
     );
   }
 }
